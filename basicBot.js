9850eee549a532107a432949d6aa0469154f18fa
Asfargo <Asfargo@semEmail.com>
2015-08-29 14:42:25 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2356,7 +2356,7 @@
 
             rouletteCommand: {
                 command: 'roulette',
-                rank: 'user',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
