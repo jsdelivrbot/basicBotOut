844231528071b9ff495a775da7c4b4edd08f1bf9
Asfargo <Asfargo@semEmail.com>
2015-08-29 19:51:28 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1518,7 +1518,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
-                    API.sendChat(' Are you trying kill me? ');
+                    API.sendChat(' /me Are you trying kill me? ');
                     }
                 }
             },
